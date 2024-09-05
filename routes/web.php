0a525d638f15fc943da72aa377e41036d5c9d1ae
danglinh152 <danglinh.k4@gmail.com>
2024-09-05 10:39:00 +0700

    9/5/2024

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login-admin', function () {
+    return view('login-admin-page');
+});
+
+Route::get('/admin-page', function () {
+    return view('admin-page');
+});
+
