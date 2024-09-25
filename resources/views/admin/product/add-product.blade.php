b56cf5c91fb05f49746d4c484f997e87195e56a0
danglinh152 <danglinh.k4@gmail.com>
2024-09-25 14:40:09 +0700

    temp

--- resources/views/admin/product/add-product.blade.php
+++ resources/views/admin/product/add-product.blade.php
@@ -28,8 +28,7 @@
                         description</label>
                     <div class="mt-2">
                         <textarea name="product_long_desc"
-                            class="font-medium mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
-                                    </textarea>
+                            class="font-medium mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></textarea>
                     </div>
                 </div>
                 <div class="sm:col-span-3 mt-4">
