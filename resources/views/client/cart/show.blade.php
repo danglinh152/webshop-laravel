3d4c801d7412bc0b0040e28c9cb6d9f3ba5e32a7
ledat139 <tiendatcntt1307@gmail.com>
2024-10-01 01:34:12 +0700

    checkbox cartdetail and price total

--- resources/views/client/cart/show.blade.php
+++ resources/views/client/cart/show.blade.php
@@ -1,127 +1,122 @@
 @extends('client.layout.homepage-layout')
 @section('cartShow')
-<div class="container-fluid py-5">
-    <div class="container py-5">
-        <div class="mb-3">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="/">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Chi Tiết Giỏ Hàng</li>
-                </ol>
-            </nav>
-        </div>
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Sản phẩm</th>
-                        <th scope="col">Tên</th>
-                        <th scope="col">Giá cả</th>
-                        <th scope="col">Số lượng</th>
-                        <th scope="col">Thành tiền</th>
-                        <th scope="col">Xử lý</th>
-                    </tr>
-                </thead>
-                <tbody>
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="mb-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Chi Tiết Giỏ Hàng</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <th scope="row">
-                                <div class="d-flex align-items-center">
-                                    <img src="{{asset('public/backend/products-images/47920_asus_tuf_gaming_a15_fa506nc_hn011w_anphatcomputer_1.jpg')}}"
-                                        class="img-fluid me-5 rounded-circle"
-                                        style="width: 80px; height: 80px;" alt="">
-                                </div>
-                            </th>
-                            <td>
-                                <p class="mb-0 mt-4">
-                                    <a href="{{URL::to('/product/id')}}" target="_blank">
-                                        Laptop Asus Tuf gaming
-                                    </a>
-                                </p>
-                            </td>
-                            <td>
-                                <p class="mb-0 mt-4">
-                                     19,000,000đ
-                                </p>
-                            </td>
-                            <td>
-                                <div class="input-group quantity mt-4" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button
-                                            class="btn btn-sm btn-minus rounded-circle bg-light border">
-                                            <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text"
-                                        class="form-control form-control-sm text-center border-0"
-                                        value="0"
-                                        data-cart-detail-id="${cartDetail.id}"
-                                        data-cart-detail-price="${cartDetail.price}"
-                                        data-cart-detail-index="${status.index}">
-                                    <div class="input-group-btn">
-                                        <button
-                                            class="btn btn-sm btn-plus rounded-circle bg-light border">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <p class="mb-0 mt-4" data-cart-detail-id="">
-                                    <fmt:formatNumber type="number"
-                                        value="" /> 19,000,000 đ
-                                </p>
-                            </td>
-                            <td>
-                                <form method="post"
-                                    action="/delete-product-from-cart/${cartDetail.id}">
-                                    <button class="btn btn-md rounded-circle bg-light border mt-4">
-                                        <i class="fa fa-times text-danger"></i>
-                                    </button>
-                                </form>
-                            </td>
+                            <th scope="col">Sản phẩm</th>
+                            <th scope="col">Tên</th>
+                            <th scope="col">Giá cả</th>
+                            <th scope="col">Số lượng</th>
+                            <th scope="col">Thành tiền</th>
+                            <th scope="col">Xử lý</th>
                         </tr>
-                    </c:forEach>
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        @for ($i = 0; $i < 2; $i++)
+                            <tr>
+                                <th scope="row" class="d-flex align-items-center gap-3">
+                                    <div class="checkCart">
+                                        <input class="form-check-input" type="checkbox" name=""
+                                            index="{{ $i }}" id="check-cart-detail{{ $i }}">
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <img src="{{ asset('public/backend/products-images/47920_asus_tuf_gaming_a15_fa506nc_hn011w_anphatcomputer_1.jpg') }}"
+                                            class="img-fluid me-5 rounded-circle" style="width: 80px; height: 80px;"
+                                            alt="">
+                                    </div>
+                                </th>
+                                <td>
+                                    <p class="mb-0 mt-4">
+                                        <a href="{{ URL::to('/product/id') }}" target="_blank">
+                                            Laptop Asus Tuf gaming
+                                        </a>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p class="mb-0 mt-4" id="price{{ $i }}">
+                                        19,000,000
+                                    </p>
+                                </td>
+                                <td>
+                                    <div class="input-group quantity mt-4" style="width: 100px;">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-sm btn-minus rounded-circle bg-light border mt-0">
+                                                <i class="fa fa-minus"></i>
+                                            </button>
+                                        </div>
+                                        <input type="text" class="form-control form-control-sm text-center border-0"
+                                            value="1" data-cart-detail-id="" data-cart-detail-price="19000000"
+                                            data-cart-detail-index="{{ $i }}" disabled
+                                            style="background: transparent">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-sm btn-plus rounded-circle bg-light border mt-0">
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="mb-0 mt-4" id="total{{ $i }}" data_cart_detail_total="19000000">
+                                        19,000,000 đ
+                                    </p>
+                                </td>
+                                <td>
+                                    <form method="post" action="/delete-product-from-cart/${cartDetail.id}">
+                                        <button class="btn btn-md rounded-circle bg-light border mt-4">
+                                            <i class="fa fa-times text-danger"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endfor
+                    </tbody>
+                </table>
+            </div>
             <div class="mt-5 row g-4 justify-content-start">
-                <div class="col-md-6"></div>
-                <div class="col-12 col-md-6">
+                <div class="col-md-7"></div>
+                <div class="col-12 col-md-5">
                     <div class="bg-light rounded">
                         <div class="p-4">
-                            <h2 class=" mb-4 fw-normal">Thông Tin Đơn
-                                Hàng</span>
-                                </h1>
-                                <div class="d-flex justify-content-between mb-4">
-                                    <h5 class="mb-0 me-4">Tạm tính:</h5>
-                                    <p class="mb-0" data-cart-total-price="${priceTotal}">
-                                        <fmt:formatNumber type="number" value="${priceTotal}" /> đ
-                                    </p>
+                            <h4 class="mb-4 ">Thông tin đơn hàng
+                            </h4>
+                            <div class="d-flex justify-content-between mb-4 text-dark">
+                                <p class="mb-0 me-4">Tạm tính:</p>
+                                <p class="mb-0 " data-cart-total-price="0">
+                                    0 đ
+                                </p>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <p class="mb-0 me-4 text-dark">Phí vận chuyển:</p>
+                                <div class="">
+                                    <p class="mb-0 text-dark">0 đ</p>
                                 </div>
-                                <div class="d-flex justify-content-between">
-                                    <h5 class="mb-0 me-4">Phí vận chuyển</h5>
-                                    <div class="">
-                                        <p class="mb-0">0 đ</p>
-                                    </div>
-                                </div>
+                            </div>
                         </div>
-                        <div
-                            class="py-4 mb-4 border-top border-bottom d-flex justify-content-between">
-                            <h5 class="mb-0 ps-4 me-4">Tổng số tiền</h5>
-                            <p class="mb-0 pe-4" data-cart-total-price="${priceTotal}">
-                                <fmt:formatNumber type="number" value="${priceTotal}" /> đ
+                        <div class="py-4 mb-4 border-top border-bottom d-flex justify-content-between">
+                            <p class="mb-0 ps-4 me-4 text-dark">Tổng số tiền:</p>
+                            <p class="mb-0 pe-4 text-dark" data-cart-total-price="0">
+                                0 đ
                             </p>
                         </div>
-                        <form action="" method="post">
-                            
-                            <button
-                                class="btn border-secondary rounded-pill px-4 py-3 text-primary text-uppercase mb-4 ms-4">Xác
-                                nhận thanh toán
+                        <form action="{{ URL::to('/checkout') }}" method="get">
+                            <button class="btn border-secondary rounded-pill px-4 py-3 text-primary  mb-4 ms-4">Xác
+                                nhận đơn hàng
                             </button>
                         </form>
                     </div>
                 </div>
             </div>
+        </div>
     </div>
-</div>
 @stop
--- resources/views/client/layout/homepage-layout.blade.php
+++ resources/views/client/layout/homepage-layout.blade.php
@@ -11,26 +11,24 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{asset('public/frontend/client/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet">
-    <link href="{{asset('public/frontend/client/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('public/frontend/client/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/frontend/client/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
 
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('public/frontend/client/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('public/frontend/client/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-    <link href="{{asset('public/frontend/client/css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('public/frontend/client/css/style.css') }}" rel="stylesheet">
     <style>
         dialog {
             padding: 20px;
@@ -55,12 +53,12 @@ $avatar = session('image');
 
 <body>
     <?php if ($message): ?>
-        <dialog id="forget-password-dialog">
-            <h2>Welcome Back to 10pm Store!</h2>
-            <p><?php echo $message; ?></p>
-            <button id="close-dialog">Close</button>
-        </dialog>
-        <?php Session::put('message', null); ?>
+    <dialog id="forget-password-dialog">
+        <h2>Welcome Back to 10pm Store!</h2>
+        <p><?php echo $message; ?></p>
+        <button id="close-dialog">Close</button>
+    </dialog>
+    <?php Session::put('message', null); ?>
     <?php endif; ?>
 
     <!-- Spinner  -->
@@ -74,20 +72,21 @@ $avatar = session('image');
     <div class="container-fluid fixed-top shadow">
         <div class="container px-0">
             <nav class="navbar navbar-light bg-white navbar-expand-xl">
-                <a href="{{URL::to('/')}}" class="navbar-brand">
-                    <img style="width: 200px; height: 85px; border-radius:20%; object-fit:cover" src="{{asset('public/frontend/client/img/logo.png')}}" alt="">
+                <a href="{{ URL::to('/') }}" class="navbar-brand">
+                    <img style="width: 200px; height: 85px; border-radius:20%; object-fit:cover"
+                        src="{{ asset('public/frontend/client/img/logo.png') }}" alt="">
                 </a>
                 <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                     <div class="navbar-nav mx-auto me-5">
-                        <a href="{{URL::to('')}}" class="nav-item nav-link active"
+                        <a href="{{ URL::to('') }}" class="nav-item nav-link active"
                             style="font-weight: 600; font-size: 16px;">Trang Chủ</a>
-                        <a href="{{URL::to('/product')}}" class="nav-item nav-link"
+                        <a href="{{ URL::to('/product') }}" class="nav-item nav-link"
                             style="font-weight: 600; font-size: 16px;">Sản Phẩm</a>
                         <a href="" class="nav-item nav-link" style="font-weight: 600; font-size: 16px;">Liên
                             hệ</a>
                     </div>
                     <div class="d-flex">
-                        <a href="{{URL::to('/cart')}}" class="position-relative me-4 my-auto">
+                        <a href="{{ URL::to('/cart') }}" class="position-relative me-4 my-auto">
                             <i class="fa fa-shopping-bag fa-2x"></i>
                             <span
                                 class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1"
@@ -96,48 +95,48 @@ $avatar = session('image');
                             </span>
                         </a>
                         @if (Session::has('user_name'))
-                        <div class="dropdown my-auto">
-                            <a href="#" class="dropdown" role="button" id="dropdownMenuLink"
-                                data-bs-toggle="dropdown" aria-expanded="false" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                <i class="fas fa-user fa-2x"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-end p-4" labelledby="dropdownMenuLink">
-                                <li class="d-flex align-items-center flex-column" style="min-width: 250px;">
-                                    @if ($avatar)
-                                    <img class=""
-                                        src="{{ $avatar }}"
-                                        alt="" aria-hidden="true"
-                                        style="height: 200px; width: 200px; object-fit: cover; border-radius: 50%" />
-                                    @else
-                                    <img class=""
-                                        src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
-                                        alt="" aria-hidden="true"
-                                        style="height: 200px; width: 200px; object-fit: cover; border-radius: 50%" />
-                                    @endif
+                            <div class="dropdown my-auto">
+                                <a href="#" class="dropdown" role="button" id="dropdownMenuLink"
+                                    data-bs-toggle="dropdown" aria-expanded="false" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    <i class="fas fa-user fa-2x"></i>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end p-4" labelledby="dropdownMenuLink">
+                                    <li class="d-flex align-items-center flex-column" style="min-width: 250px;">
+                                        @if ($avatar)
+                                            <img class="" src="{{ $avatar }}" alt=""
+                                                aria-hidden="true"
+                                                style="height: 200px; width: 200px; object-fit: cover; border-radius: 50%" />
+                                        @else
+                                            <img class=""
+                                                src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
+                                                alt="" aria-hidden="true"
+                                                style="height: 200px; width: 200px; object-fit: cover; border-radius: 50%" />
+                                        @endif
 
-                                    <div class="text-dark fw-bold fs-5 my-3">
-                                        {{ Session::get('user_name') }}
-                                    </div>
-                                </li>
+                                        <div class="text-dark fw-bold fs-5 my-3">
+                                            {{ Session::get('user_name') }}
+                                        </div>
+                                    </li>
 
-                                <li><a class="dropdown-item" href="#">Quản lý tài khoản</a></li>
-                                <li><a class="dropdown-item" href="/order-history">Lịch sử mua hàng</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li>
-                                    <!-- <form action="{{URL::to('/logout')}}" method="post">
+                                    <li><a class="dropdown-item" href="#">Quản lý tài khoản</a></li>
+                                    <li><a class="dropdown-item" href="/order-history">Lịch sử mua hàng</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li>
+                                        <!-- <form action="{{ URL::to('/logout') }}" method="post">
                                             <button class="dropdown-item" href="#">Đăng xuất</button>
                                         </form> -->
-                                    <a class="dropdown-item" href="{{URL::to('/logout')}}">Đăng xuất</a>
-                                </li>
-                            </ul>
-                        </div>
+                                        <a class="dropdown-item" href="{{ URL::to('/logout') }}">Đăng xuất</a>
+                                    </li>
+                                </ul>
+                            </div>
                     </div>
-                    @elseif(!Session::has('user_name'))
+                @elseif(!Session::has('user_name'))
                     <div class="navbar-nav mx-auto me-5">
-                        <a class="nav-item nav-link active" style="font-weight: 600; font-size: 16px;" href="{{ URL::to('/login') }}">
+                        <a class="nav-item nav-link active" style="font-weight: 600; font-size: 16px;"
+                            href="{{ URL::to('/login') }}">
                             Đăng nhập
                         </a>
                     </div>
@@ -145,7 +144,7 @@ $avatar = session('image');
 
 
                     <!-- <div class="navbar-nav mx-auto me-5">
-                        <a class="nav-item nav-link active" style="font-weight: 600; font-size: 16px;" href="{{URL::to('/login')}}">Đăng nhập</a>
+                        <a class="nav-item nav-link active" style="font-weight: 600; font-size: 16px;" href="{{ URL::to('/login') }}">Đăng nhập</a>
                     </div> -->
                 </div>
             </nav>
@@ -219,27 +218,25 @@ $avatar = session('image');
 
 
     <!-- Back to Top -->
-    <a href="#"
-        class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i
+    <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i
             class="fa fa-arrow-up"></i></a>
 
 
     <!-- JavaScript Libraries -->
-    <script
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('/public/frontend/client/lib/easing/easing.min.js')}}"></script>
-    <script src="{{asset('/public/frontend/client/lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{asset('/public/frontend/client/lib/lightbox/js/lightbox.min.js')}}"></script>
-    <script src="{{asset('/public/frontend/client/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('/public/frontend/client/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('/public/frontend/client/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('/public/frontend/client/lib/lightbox/js/lightbox.min.js') }}"></script>
+    <script src="{{ asset('/public/frontend/client/lib/owlcarousel/owl.carousel.min.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-    <script src="{{asset('/public/frontend/client/js/swiper-scripts.js')}}"></script>
+    <script src="{{ asset('/public/frontend/client/js/swiper-scripts.js') }}"></script>
 
     <!-- Template Javascript -->
-    <script src="{{asset('/public/frontend/client/js/main.js')}}"></script>
+    <script src="{{ asset('/public/frontend/client/js/main.js') }}"></script>
+    {{-- <script src="{{ asset('/public/frontend/client/js/checkboxCart.js') }}"></script> --}}
     <script>
         // Check if the dialog exists and show it if there is a message
         window.onload = function() {
