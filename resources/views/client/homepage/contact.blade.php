82197cd4e5d9d81e7a977f1cbc852e08077d899d
Huynh Nhat Duy <huynhnhatduy1824@gmail.com>
2024-12-23 01:55:39 +0700

    redesign contact

--- resources/views/client/homepage/contact.blade.php
+++ resources/views/client/homepage/contact.blade.php
@@ -3,11 +3,22 @@
     <!-- Contact Start -->
     <div class="container-fluid contact py-5">
         <div class="container py-5">
+
+
             <div class="p-5 bg-light rounded">
+                <div class="row mb-5">
+                    <div class="col-12 text-center">
+                        <h1 class="text-primary" style="font-size: 50px; font-family: 'Open Sans', sans-serif; font-weight: 700; margin-bottom: 30px;">Giới thiệu cửa hàng</h1>
+                        <p class="lead mb-4" style="text-align: justify;">Chào mừng bạn đến với <b class="text-primary">10PM STORE</b>, nơi cung cấp những sản phẩm điện tử chất lượng cao với giá cả hợp lý. Chúng tôi chuyên cung cấp các thiết bị điện tử chính hãng như điện thoại, laptop, máy tính bảng và nhiều sản phẩm công nghệ khác. Với sứ mệnh mang đến cho khách hàng những sản phẩm tiên tiến, bền bỉ và tiện ích, chúng tôi cam kết luôn mang lại trải nghiệm mua sắm tuyệt vời, dịch vụ khách hàng tận tâm và giao hàng nhanh chóng.
+                            Chúng tôi hiểu rằng công nghệ là một phần không thể thiếu trong cuộc sống hiện đại, và chúng tôi luôn nỗ lực không ngừng để cung cấp những sản phẩm tốt nhất với giá cả cạnh tranh nhất. Tại <b class="text-primary">10PM Store</b>, bạn sẽ tìm thấy những thiết bị điện tử phù hợp với mọi nhu cầu từ học tập, làm việc cho đến giải trí.
+                            Chúng tôi rất hân hạnh được phục vụ bạn và hy vọng rằng bạn sẽ tìm thấy những sản phẩm ưng ý tại cửa hàng của chúng tôi.</p>
+                        <iframe width="100%" height="600" src="https://www.youtube.com/embed/NNerzOwb8ew" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
                 <div class="row g-4">
                     <div class="col-12">
                         <div class="text-center mx-auto" style="max-width: 700px;">
-                            <h1 class="text-primary">Liên hệ với chúng tôi</h1>
+                            <h1 class="text-primary" style="font-size: 50px; font-family: 'Open Sans', sans-serif; font-weight: 700;">Liên hệ với chúng tôi</h1>
                             <p class="mb-4">Dưới đây là một số thông tin của chúng tôi. Hãy liên hệ khi cần thiết</p>
                         </div>
                     </div>
